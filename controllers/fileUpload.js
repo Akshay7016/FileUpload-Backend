79da1489e87158c7971ddbc318e73fceb3fc1ca4
Akshay7016 <asajgule@gmail.com>
2023-05-08 20:34:02 +0530

    imageUpload api added

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -1,4 +1,5 @@
 const File = require("../models/File");
+const cloudinary = require("cloudinary").v2;
 
 exports.localFileUpload = async (req, res) => {
     try {
@@ -26,3 +27,55 @@ exports.localFileUpload = async (req, res) => {
         })
     }
 };
+
+const isFileTypeSupported = (type, supportedTypes) => {
+    return supportedTypes.includes(type);
+};
+
+const uploadFileToCloudinary = async (file, folder) => {
+    const options = { folder }
+    return await cloudinary.uploader.upload(file.tempFilePath, options)
+}
+
+exports.imageUpload = async (req, res) => {
+    try {
+        const { name, email, tags } = req.body;
+
+        const file = req.files.imageFile;
+
+        const supportedTypes = ["jpg", "jpeg", "png"];
+        const fileType = file.name.split(".")[1].toLowerCase();
+
+        // if file type is not supported
+        if (!isFileTypeSupported(fileType, supportedTypes)) {
+            return res.status(400).json({
+                success: false,
+                message: "File type not supported!"
+            })
+        }
+
+        // file type is supported
+        const response = await uploadFileToCloudinary(file, "Akshay");
+        // console.log(response);
+
+        // save entry in database
+        await File.create({
+            name,
+            email,
+            tags,
+            imageUrl: response.secure_url
+        })
+
+        res.status(200).json({
+            success: true,
+            imageUrl: response.secure_url,
+            message: "Image uploaded successfully!"
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: "Something went wrong: " + error.message
+        })
+    }
+} 
--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 // middleware for file uploading
 const fileupload = require("express-fileupload");
-app.use(fileupload());
+app.use(fileupload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
+}));
 
 // Database connection
 const dbConnect = require("./config/database");
