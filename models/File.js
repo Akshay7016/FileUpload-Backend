4c34b02bcdce689a89e4d651b258e7cdc8fe1117
Akshay7016 <asajgule@gmail.com>
2023-05-10 19:36:31 +0530

    nodemailer added

--- models/File.js
+++ models/File.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const fileSchema = new mongoose.Schema({
     name: {
@@ -16,5 +18,29 @@ const fileSchema = new mongoose.Schema({
     }
 
 });
+
+// Post middleware
+fileSchema.post("save", async (doc) => {
+    try {
+        // Create transporter
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS
+            }
+        });
+
+        // send mail
+        await transporter.sendMail({
+            from: "Test Nodemailer",
+            to: doc.email,
+            subject: "New file uploaded successfully",
+            html: `<h2>Dear ${doc.name},</h2> <p>File uploaded, view here: <a href="${doc.fileUrl}">${doc.fileUrl}</a></p>`
+        })
+    } catch (error) {
+        console.log("Something went wrong while sending mail: ", error.message);
+    }
+})
 
 module.exports = mongoose.model("File", fileSchema);
